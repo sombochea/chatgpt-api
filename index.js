7bf11dbac22aef11c6b73a717508c883e0a0ca71
Sambo Chea <sombochea@cubetiqs.com>
2023-02-17 12:39:28 +0700

    Updated

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import server from './server.js';
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port', port);
 });
